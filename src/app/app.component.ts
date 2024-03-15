40393bb5a11c9465adf13f0cdb57c0df28e610b0
Paulin <paulingaillot@gmail.com>
2024-03-15 09:22:58 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FRONT_2324-PROJET_ROLO_GAILLOT-GITHUB';
+}
